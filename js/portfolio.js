2b98e51d32db32cda429e886281813f1b977dcbf
unknown <saimang12@naver.com>
2023-03-21 19:47:17 +0900

    2023-03-21

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,3 +1,4 @@
+// jQuery 시작
 $(function () {
 
     var fullpageOption = {
@@ -14,33 +15,59 @@ $(function () {
 
     $('.main').fullpage(fullpageOption);
 
-
-
-
     $('.mopen').on('click', function () {
         $(this).toggleClass('on');
         $('.cover').toggleClass('on');
-        // $('.header').toggleClass('on');
     });
 
 
     $('.cover li').on('click', function () {
         $('.cover').removeClass('on');
         $('.mopen').removeClass('on');
-        // $('.header').removeClass('on');
     });
 
 
     $('.cover').on('wheel', function (e) {
         e.stopPropagation();
-
-
     });
 
 
+    $(document).ready(function () {
+        const stars = $('.stars');
+        const starsNum = 100;
+
+        for (let i = 0; i < starsNum; i++) {
+            const star = $('<div></div>');
+            star.addClass('star');
+            star.css({
+                left: Math.random() * 100 + 'vw',
+                top: Math.random() * 100 + 'vh',
+                animationDelay: Math.random() * 3 + 's',
+                animationDuration: Math.random() * 3 + 1.5 + 's'
+            });
+            stars.append(star);
+        }
+
+        $('.slogan p').addClass('on');
+        $('.slogan .sub_tit').addClass('on');
+    });
+    // 무작위 좌표에 생성된 100개의 점이 반짝이는 효과 
+});
+
+// jQuery 끝
 
 
+// 바닐라 자바스크립트 시작
+document.addEventListener("DOMContentLoaded", function () {
+    new TypeIt(".slogan>h2", {
+        speed: 150,
+    })
+        .type('experience')
+        .pause(300)
+        .type(' , ')
+        .pause(500)
+        .type('develop')
+        .go();
+});
 
-
-
-})
+// 바닐라 자바 스크립트 끝
